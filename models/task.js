ff9bd4ac88f5f371f053da68aee423e84c18bf0f
kavyaD01 <kavyadilli01@gmail.com>
2024-01-31 17:52:01 +0530

    first commit

---
+++ models/task.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+// creating Schema for Tasks
+const taskSchema = new mongoose.Schema({
+description: {
+type: String,
+required: true
+},
+category: {
+type: String,
+required: false
+},
+date: {
+type: Date,
+required: false
+}
+});
+const Task = mongoose.model('Task', taskSchema);
+// exporting the Schema
+module.exports = Task;
